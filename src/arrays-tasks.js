0fd7db2619c3684ed0b5605b5c0c52a812b1e2b3
Aleks Gomeniuk <agsfinks@gmail.com>
2025-01-03 19:04:17 +0000

    feat: propagateItemsByPositionIndex reduce array.fill

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -532,8 +532,13 @@ function findLongestIncreasingSubsequence(nums) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.reduce((acc, item, index) => {
+    const position = index + 1;
+    const repeatedItem = Array(position).fill(item);
+    acc.push(...repeatedItem);
+    return acc;
+  }, []);
 }
 
 /**
